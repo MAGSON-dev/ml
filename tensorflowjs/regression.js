4cb35157b29b917b2c0fb85eb0b9876249dcaf2e
sondresorbye0111 <sondre@magson.no>
2018-06-11 22:10:44 +0200

    done with commenting

--- tensorflowjs/regression.js
+++ tensorflowjs/regression.js
@@ -25,14 +25,14 @@ function loss(pred, labels) { // loss function: mean square error
     return pred.sub(labels).square().mean();
 }
 
-function predict(x) { 
-    const xs = tf.tensor1d(x); // makes a tensor from the x coordinate
+function predict(x) { // takes an array of x coordinates as a parameter and returns the predicted ys as a tensor1d
+    const xs = tf.tensor1d(x); // makes a tensor from the x coordinates
     // y = mx + b;
-    const ys = xs.mul(m).add(b);
+    const ys = xs.mul(m).add(b); // ys is a tensor1d with all predictions based on the x_vals
     return ys; // return the predicted y value
 }
 
-function mousePressed() {
+function mousePressed() { // save coordinates
     let x = map(mouseX, 0, width, 0, 1);
     let y = map(mouseY, 0, height, 1, 0);
     x_vals.push(x);
@@ -41,10 +41,10 @@ function mousePressed() {
 
 function draw() {
 
-    tf.tidy(() => {
+    tf.tidy(() => { // delete all tensors when finished
         if (x_vals.length > 0) { // make sure you have added points before making predictions
-            const ys = tf.tensor1d(y_vals);
-            optimizer.minimize(() => loss(predict(x_vals), ys));
+            const ys = tf.tensor1d(y_vals); // convert the y_vals array to a tensor1d
+            optimizer.minimize(() => loss(predict(x_vals), ys)); // train the optimizer (sgd) using the loss function 
         }
     });
 
@@ -52,17 +52,17 @@ function draw() {
 
     stroke(255);
     strokeWeight(8);
-    for (let i = 0; i < x_vals.length; i++) {
+    for (let i = 0; i < x_vals.length; i++) { // adding all points to the canvas
         let px = map(x_vals[i], 0, 1, 0, width);
         let py = map(y_vals[i], 0, 1, height, 0);
         point(px, py);
     }
 
-    const lineX = [0, 1];
+    const lineX = [0, 1]; // the x values for the regression line 
 
-    const ys = tf.tidy(() => predict(lineX));
-    let lineY = ys.dataSync();
-    ys.dispose();
+    const ys = tf.tidy(() => predict(lineX)); // the predicted ys from the xs from the regression line
+    let lineY = ys.dataSync(); // get the raw data from the tensor 
+    ys.dispose(); // delete the tensor to prevent memory leaks
 
     let x1 = map(lineX[0], 0, 1, 0, width);
     let x2 = map(lineX[1], 0, 1, 0, width);
@@ -71,7 +71,7 @@ function draw() {
     let y2 = map(lineY[1], 0, 1, height, 0);
 
     strokeWeight(2);
-    line(x1, y1, x2, y2);
+    line(x1, y1, x2, y2); // draw the regression line
 
-    console.log(tf.memory().numTensors);
+    // console.log(tf.memory().numTensors); 
 }
